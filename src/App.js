815c31300df7902c727799dadf2ef28f279c81a1
Aziz Karimov <azez.karimov@gmail.com>
2023-10-20 19:12:53 +0400

    AK/FEATURE/MEDIAQUERY-PANEL

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useLayoutEffect } from "react";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Portfolio from "./pages/Portfolio/Portfolio";
 
 function App() {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, behavior: "auto" });
+  }, [pathname]);
+
   return (
     <>
       <div className="content">
